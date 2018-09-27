1e39146fbcbebb164d7ddd2cf4eb4f1203497cf4
ljuns <ljuns.dev@gmail.com>
2018-09-27 14:38:46 +0800

    Observable 对象

--- app/src/main/java/com/itscoder/ljuns/practise/databinding/model/User.java
+++ app/src/main/java/com/itscoder/ljuns/practise/databinding/model/User.java
@@ -1,11 +1,15 @@
 package com.itscoder.ljuns.practise.databinding.model;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import com.itscoder.ljuns.practise.BR;
+
 /**
  * @author ljuns
  * Created at 2018/9/20.
  * I am just a developer.
  */
-public class User {
+public class User extends BaseObservable {
     private String mName;
     private int mAge;
 
@@ -14,12 +18,24 @@ public class User {
         mAge = age;
     }
 
+    @Bindable
     public String getName() {
         return mName;
     }
 
+    @Bindable
     public int getAge() {
         return mAge;
+    }
+
+    public void setName(String name) {
+        mName = name;
+        notifyPropertyChanged(BR.name);
+    }
+
+    public void setAge(int age) {
+        mAge = age;
+        notifyPropertyChanged(BR.age);
     }
 }
 
