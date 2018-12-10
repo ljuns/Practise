a3c53dd5966014c8c13597ee59b43b3917c584f9
ljuns <ljuns.dev@gmail.com>
2018-12-10 22:20:29 +0800

    可空性

--- app/src/main/java/com/itscoder/ljuns/practise/kotlin/chapter6/NullAble.kt
+++ app/src/main/java/com/itscoder/ljuns/practise/kotlin/chapter6/NullAble.kt
@@ -4,3 +4,31 @@ package com.itscoder.ljuns.practise.kotlin.chapter6
  * Created by ljuns at 2018/12/10.
  * I am just a developer.
  */
+
+fun main(args: Array<String>) {
+    val s: String? = " "
+
+    /**
+     * 针对 null
+     */
+    // null 时返回 "abc"，否则返回自己
+    println(s ?: "abc")
+
+    // null 时不执行，否则执行
+    println(s?.toUpperCase())
+
+    // null 时抛异常，否则执行
+    println(s!!.toUpperCase())
+
+    // null 时不执行并返回自己，否则执行并返回结果
+    s?.let { println("it = $it") }
+
+    /**
+     * 针对 null 和空字符串（" "）
+     */
+    // s == null || s.length == 0 || s.trim() == ""
+    println(s.isNullOrBlank())
+
+    // s == null || s.length == 0
+    println(s.isNullOrEmpty())
+}
