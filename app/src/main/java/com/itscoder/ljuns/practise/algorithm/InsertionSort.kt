7b40bdc7d676e98e754001a5ebba628038fbb533
ljuns <ljuns.dev@gmail.com>
2019-01-05 15:22:37 +0800

    插入排序

--- app/src/main/java/com/itscoder/ljuns/practise/algorithm/InsertionSort.kt
+++ app/src/main/java/com/itscoder/ljuns/practise/algorithm/InsertionSort.kt
@@ -4,6 +4,11 @@ package com.itscoder.ljuns.practise.algorithm
  * Created by ljuns at 2019/1/5.
  * I am just a developer.
  * 插入排序
+ * 从第 2 个元素开始和前面的元素比较并排序
+ * 例如：1, 3, 2
+ * 1、第一次 for 循环：3 和 1 比较，不需要改变
+ * 2.1、第二次 for 循环：2 和 3 比较，调换位置变成 1, 2, 3，此时 j == 1
+ * 2.2、2 再和 1 比较，不需要改变，排序结束
  */
 
 class InsertionSort {
@@ -16,6 +21,7 @@ class InsertionSort {
     private fun sort(arr: IntArray) {
         for (i in arr.indices) {
             var j = i
+            // 判断 j > 0 有两层意义：1、第一个元素不用比较；2、后面的比较排序到第 1 个元素时停止
             while (j > 0 && arr[j] < arr[j - 1]) {
                 val temp = arr[j]
                 arr[j] = arr[j - 1]
